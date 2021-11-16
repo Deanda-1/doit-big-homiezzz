a737f7d593308fe0ba834c54c01fc3c5f84a1b1e
Antonio Marino <marino.antonio375@gmail.com>
2021-11-16 13:30:35 -0500

    got homepage to load

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-// Import express package
 const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
 
-// our local port 
 const PORT = 3001;
 
-// Initialize our app variable by setting it to the value of express()
 const app = express();
 
-// route to the homepage
-app.get('/', (req, res) => res.send('homepage'));
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
 
+// app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/drink_catalog/main/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/recipe/main/index.html'))
+);
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
