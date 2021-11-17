d6b6e3b0443596e3776a3bf5293947b02807d816
Matthew Dukeshire <mdukeshire07@gmail.com>
2021-11-17 13:47:33 -0500

    fixed routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./recipeRoutes');
+const recipeRoutes = require('./recipeRoutes');
 
 router.use('/', recipeRoutes);
-router.use('/api', apiRoutes);
+
 
 module.exports = router;
